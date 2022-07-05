b5b04c377a3e3410974e02663314e3c8aca77624
ibktommy <atomquartz@gmail.com>
2022-07-05 15:56:22 +0100

    Create structure and styling of project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+import Questions from './Questions/Questions';
 
 function App() {
   return (
     <>
-      <div className="content">
-        <div className="box">
+      <div className="content center">
+        <div className="box center">
           <div className="left">
             <h2 className="title">Questions And Answers About Login</h2>
           </div>
           <div className="right">
-
+            <Questions />
           </div>
         </div>
       </div>
--- src/Questions/Questions.jsx
+++ src/Questions/Questions.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+// import data from "../data";
+import "./Questions.css";
 
 const Questions = () => {
-  return (
-    <>
-      
-    </>
-  )
-}
+	return (
+		<>
+			<div className="card center">
+				<div className="card-question center">
+					<h2 className="question-title">
+						Do i have to allow the use of cookies?
+					</h2>
+					<i class="fa-solid fa-plus"></i>
+				</div>
+
+				<div className="answer">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+					molestias obcaecati dolores nam in error!
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Questions
