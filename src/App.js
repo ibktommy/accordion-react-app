cfa3edefca855865c04c5fe2d8c67b77371c585f
ibktommy <atomquartz@gmail.com>
2022-07-05 15:27:19 +0100

    Created project files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="content">
+        <div className="box">
+          <div className="left">
+            <h2 className="title">Questions And Answers About Login</h2>
+          </div>
+          <div className="right">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
